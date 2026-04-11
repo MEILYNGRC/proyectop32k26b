dc817ed54d24c913e7ae6cfbf8b84bdc2f31fa56
Marco Hernandez <mhernandezt23@miumg.edu.gt>
2026-04-10 22:53:42 -0600

    Actualización en mantenimiento de usuarios

--- proyectop32k26b/src/main/java/Scripts/sigLmdLogistica.sql
+++ proyectop32k26b/src/main/java/Scripts/sigLmdLogistica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2026 at 07:32 PM
+-- Generation Time: Apr 09, 2026 at 05:59 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,16 +18,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: sig
+-- Database: `sig`
 --
 
 --
--- Dumping data for table aplicaciones
+-- Dumping data for table `aplicaciones`
 --
 
-INSERT INTO aplicaciones (Aplcodigo, Aplnombre, Aplestado) VALUES
-(2, 'barcelonaFansApp', '1'),
-(3, 'eBooksGuatemala', '1');
+INSERT INTO `aplicaciones` (`Aplcodigo`, `Aplnombre`, `Aplestado`) VALUES
+(10001, 'Mantenimiento Usuarios', '1'),
+(10002, 'Mantenimiento Aplicaciones', '1');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
